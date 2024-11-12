975118683e59189a36fc43ca5d7713f08a7a25d4
i-roger <mroger.dev@gmail.com>
2024-11-12 12:52:34 -0300

    Add Benefícios

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,6 @@
+import Image from "next/image";
+import Check from "@/assets/icon/check.svg"
+
 export default function Home() {
   return (
     <div className="flex flex-col items-center gap-10 ">
@@ -22,6 +25,7 @@ export default function Home() {
       </div>
       {/* Head */}
 
+      {/* StoryTelling */}
       <div className="flex flex-col p-4 gap-4">
         <h1 className="border border-[#ab3ed8] p-4 rounded-xl text-[14px] text-justify">
           Muitos empreendedores dedicam tempo e dinheiro postando nas redes, mas
@@ -35,6 +39,49 @@ export default function Home() {
         <span className="font-extrabold">Vou ajudar você a transformar cada postagem em <span className="text-[#00e5e5]">resultado</span>.</span>
         </h1>
       </div>
+      {/* StoryTelling */}
+
+      {/* Who am i? */}
+      <div className="flex flex-col gap-4">
+      <h1 className="titulo text-center">Quem sou?</h1>
+        <div className="flex flex-col items-center gap-4 text-center">
+          <div className="rounded-full w-[96px] h-[96px] bg-blue-400"></div>
+          <h1><strong>Nathalia Azevedo</strong><br/>Publicitária</h1>
+        </div>
+        <div className="text-[14px] text-justify p-4">
+          <h2>Com 6 anos de experiência em Marketing Digital e Gestão de Tráfego, ajudo empresas a crescer nas redes e atrair clientes qualificados. Trabalhei com marcas de diferentes setores, criando campanhas estratégicas que transformam engajamento em resultados reais. Se você quer potencializar sua presença online e ver retorno imediato, conte comigo para fazer isso acontecer.</h2>
+        </div>
+      </div>
+      {/* Who am i? */}
+
+      {/* Portfólio */}
+      {/* Portfólio */}
+
+      {/* Benefícios */}
+        <div className="flex flex-col gap-4">
+          <h1 className="titulo text-center">O que você ganha com Gestão de Tráfego Pago?</h1>
+          <div className="flex flex-col p-4 gap-2">
+
+            <div className="flex gap-2">
+              <Image alt="" src={Check} width={32} height={32}/>
+              <p><strong><span className="text-[#00e5e5]">Segmentação: </span></strong>Alcançamos o público certo, no momento certo.</p>
+            </div>
+            <div className="flex gap-2">
+              <Image alt="" src={Check} width={32} height={32}/>
+              <p><strong><span className="text-[#00e5e5]">Otimização Contínua: </span></strong>Ajustes constantes para maximizar resultados.</p>
+            </div>
+            <div className="flex gap-2">
+              <Image alt="" src={Check} width={32} height={32}/>
+              <p><strong><span className="text-[#00e5e5]">Redução de Custos Desnecessários: </span></strong>Sem desperdícios – cada real investido conta.</p>
+            </div>
+            <div className="flex gap-2">
+              <Image alt="" src={Check} width={32} height={32}/>
+              <p><strong><span className="text-[#00e5e5]">Análise e Relatórios Transparentes: </span></strong>Saiba exatamente o impacto das suas campanhas.</p>
+            </div>
+
+          </div>
+        </div>
+      {/* Benefícios */}
     </div>
   );
 }
